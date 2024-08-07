6d1ee2d7365bc76d9dbd07a0eba4689df0fe0177
Tonino <toninorizzo1989@yahoo.it>
2024-08-07 18:33:43 +0200

    App done

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,22 +1,14 @@
-import { CurrencyPipe } from '@angular/common';
-import { Component, Input, input } from '@angular/core';
+import { investmentService } from './../investment.service';
+
+import { Component, computed, inject } from '@angular/core';
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css',
 })
 export class InvestmentResultsComponent {
-  results = input<
-    {
-      year: number;
-      interest: number;
-      valueEndOfYear: number;
-      annualInvestment: number;
-      totalInterest: number;
-      totalAmountInvested: number;
-    }[]
-  >();
+  private investmentService = inject(investmentService);
+
+  results = computed(() => this.investmentService.resultData());
 }
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,28 +1,27 @@
-import { Component, output, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import type { InvestmentInput } from '../investment-input.model';
+import { investmentService } from './../investment.service';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  calculate = output<InvestmentInput>();
   enteredInitialInvestement = signal('0');
   enteredAnnualInvestement = signal('0');
   enteredExpectedReturn = signal('0');
   enteredDuration = signal('0');
 
+  constructor(private investmentService: investmentService) {}
+
   onSubmit() {
-    this.calculate.emit({
+    this.investmentService.OnCalculateInvestmentResults({
       initialInvestment: +this.enteredInitialInvestement(),
       annualInvestment: +this.enteredAnnualInvestement(),
       duration: +this.enteredDuration(),
       expectedReturn: +this.enteredExpectedReturn(),
     });
+
     this.enteredAnnualInvestement.set('0');
     this.enteredExpectedReturn.set('0');
     this.enteredDuration.set('0');
